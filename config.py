653cc15588045e14d3f7fe5933dba48362c2d9ab
Saurabh Singh <saurabh123mahi@gmail.com>
2026-07-15 11:48:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-HF_API_KEY = os.getenv("HF_API_KEY", "your_huggingface_api_key")
-HF_MODEL = "meta-llama/Llama-3.2-1B-Instruct"  # Free tier model
+HF_API_KEY = os.getenv("HF_API_KEY")
+HF_MODEL = "meta-llama/Llama-3.2-1B-Instruct"  
